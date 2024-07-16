95e163cebafee8058da5dbaad007ffc9a9578532
EC2 Default User <ec2-user@ip-172-31-3-0.ap-northeast-1.compute.internal>
2024-07-16 01:50:36 +0000

    レイアウト一新

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -10,10 +10,11 @@ class PostImage < ApplicationRecord
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image
   end
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+    image
+  end
+ 
 end
